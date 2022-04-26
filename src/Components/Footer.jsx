425499e279c5f9089dad1534d8f6accec549c725
Tim Jonker <tmjonker1@outlook.com>
2022-04-26 00:32:32 -0400

    Updated footer class

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
 
 function Footer(props) {
   return (
-    <footer class="footer mt-auto py-3 bg-dark">
+    <footer class="footer mt-auto py-3 w-100 bg-dark">
       <Container>
         <p>Copyright &copy; {year}</p>
       </Container>
